0c13d784423dbb8481ebf531c86b475ea31cc023
Vinicius Borges <viniciusborges22@gmail.com>
2020-01-26 23:03:26 -0300

    Run as root when needed

--- install.sh
+++ install.sh
@@ -7,22 +7,23 @@ if ! (ln -sf $(pwd)/configs/daemon.conf $HOME/.config/pulse/); then
 fi
 
 echo "Linking asound.conf..."
-if ! (ln -sf $(pwd)/configs/asound.conf /etc/); then
-	echo "Linkage error. Maybe you didn't run this script as root?" 1>&2
+if ! (sudo ln -sf $(pwd)/configs/asound.conf /etc/); then
+	echo "Linkage error. Removing daemon.conf and exiting..." 1>&2
+	rm $HOME/.config/pulse/daemon.conf
 	exit 1
 fi
 
 echo "Restarting pulseaudio..."
 
 if ! (pulseaudio -k); then
-	echo "Error killing the pulseaudio module" 1>&2
+	echo "Error killing the pulseaudio module. Please restart it manually for this to work." 1>&2
 	exit 1
 fi
 
 sleep 3
 
 if ! (pulseaudio --start); then
-	echo "Error starting the pulseaudio module" 1>&2
+	echo "Error starting the pulseaudio module. Please restart it manually for this to work." 1>&2
 	exit 1
 fi
 
